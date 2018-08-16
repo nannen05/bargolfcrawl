f84ff88547c64192fa1146a901f2bd8483c80713
nannen05 <nannen05@gmail.com>
2018-08-16 16:50:55 -0700

    adding coming soon

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to End Of Summer (Bar) Golf Tournament</h2>
+          <h3>Coming Soon</h3>
         </div>
         {/* <p className="App-intro">
           To get started, login in using GMAIL and you will be able to update your score.
@@ -24,10 +25,11 @@ class App extends Component {
         <div className="App-login">
             {/* <div className="btn"><Link to="/login"> Login </Link></div>
              <div className="btn"><Link to="/signup"> Signup </Link></div> */}
-            <div className="btn"><Link to="/scores"> Scores </Link></div>
-            <div className="btn"><Link to="/score/nick"> Nick </Link></div>
-            <div className="btn"><Link to="/score/jason"> Jason </Link></div>
-            <div className="btn"><Link to="/course"> Course </Link></div>
+            <div className="btn coming-soon"><Link to="/scores"> Enter Score </Link></div>
+            <div className="btn coming-soon"><Link to="/scores"> Leaderboard </Link></div>
+            <div className="btn hide"><Link to="/score/nick"> Nick </Link></div>
+            <div className="btn hide"><Link to="/score/jason"> Jason </Link></div>
+            <div className="btn coming-soon"><Link to="/course"> Course Rules </Link></div>
         </div>
       </div>
     );
