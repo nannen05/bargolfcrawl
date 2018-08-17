fb24ba441969e133bd14e65d97438b66b3e169d2
nannen05 <nannen05@gmail.com>
2018-08-16 21:36:04 -0700

    removing unused code

--- src/components/Score.js
+++ src/components/Score.js
@@ -7,66 +7,6 @@ import { db } from '../firebase'
 import * as actions from "../store/actions";
 import update from 'react-addons-update';
 
-class SingleScore extends Component  {
-    constructor(props) {
-        super()
-
-        this.state = {
-            score: '',
-            calculatedScore: '',
-        }
-
-        this.handleScoreChange.bind(this);
-    }
-
-    handleScoreChange(e) {
-        let scoreValue = e.target.value
-        if(e.target.value == this.props.data.par) {
-            console.log('par')
-            scoreValue = e.target.value
-        } else if (e.target.value < this.props.data.par) {
-            if(e.target.value == this.props.data.par - 1) {
-                console.log('birdie')
-                scoreValue = e.target.value
-            } else if (e.target.value == this.props.data.par - 2) {
-                console.log('eagle')
-                scoreValue = e.target.value
-            } else if (e.target.value == this.props.data.par - 3) {
-                console.log('double eagle')
-                scoreValue = e.target.value
-            }
-        } else if (e.target.value > this.props.data.par) {
-            if(e.target.value == this.props.data.par + 1) {
-                console.log('bogey')
-                scoreValue = +e.target.value// + +1
-            } else if (e.target.value == this.props.data.par + 2) {
-                console.log('double bogey')
-                scoreValue = +e.target.value// + +2
-            } else {
-                let higherScore = e.target.value - this.props.data.par
-                scoreValue = +e.target.value// + +higherScore
-            }
-        }
-        this.setState({
-            score: scoreValue,
-            calculatedScore: scoreValue
-
-        }, () => {
-            this.props.getScoreChange(this.state.score, this.props.id);
-        })
-    }
-
-    render() {
-
-        return (
-           <div>
-              <p>Par: <span>{this.props.data.par}</span></p>
-              <input type="text" placeholder={"Score for Hole " + this.props.id} id={this.props.id} value={this.state.score} onChange={e => this.handleScoreChange(e, this.props.id)}/>
-           </div>
-        )
-    }
-}
-
 class Score extends Component {
   constructor(props) {
     super(); // or super(props) ?
@@ -75,54 +15,12 @@ class Score extends Component {
       users: null,
     };
 
-    this.getScore.bind(this)
-    this.renderScores.bind(this)
   }
 
   componentDidMount() {
     db.onceGetUsers().then(snapshot =>
       this.setState({ users: snapshot.val() })
     );
-
-    console.log(this.state)
-  }
-
-  getTotalScore() {
-      return <span>this.state.totalScore</span>
-  }
-
-  getScore(value, index) {
-
-      let newScore = this.state.score
-      newScore[index - 1].score = value
-      let updatedScore = +value + +this.state.totalScore
-      this.setState({
-          newScore
-      })
-
-      let PlayerScore = []
-      this.state.score.map((value, index) => {
-          PlayerScore.push( value.score )
-      })
-      this.setState({
-          totalScore: PlayerScore.reduce((a, b) => +a + +b)
-      })
-  }
-
-  renderScoresDB() {
-      const scores = this.state.users.map((value, index) => {
-          return <p key={index} className="player-score"><span className="cap">{value.name}</span>  <span className="right">Score: {value.totalScore}</span></p>
-      })
-
-      return scores
-  }
-
-  renderScores() {
-      const scores = this.props.state.userData.map((value, index) => {
-          return <p key={index} className="player-score"><span className="cap">{value.name}</span>  <span className="right">Score: {value.totalScore}</span></p>
-      })
-
-      return scores
   }
 
   renderSortedList(users) {
@@ -141,7 +39,6 @@ class Score extends Component {
 
   render() {
 
-    console.log(this.state)
     const { users } = this.state
 
     return (
